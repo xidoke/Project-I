124a09cea9bf8fff150d1bc6e3817e5077849512
xidoke <do.pd200154@sis.hust.edu.vn>
2024-01-03 18:45:49 +0700

    .

---
+++ messenger-app/app/actions/getUser.ts
@@ -0,0 +1,29 @@
+import prisma from "@/app/libs/prismadb"
+
+import getSession from "@/app/actions/getSession";
+
+const getUser = async () => {
+    const session = await getSession()
+
+    if (!session?.user?.email) {
+        return []
+    }
+
+    try {
+        const user = await prisma.user.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            },
+            where: {
+                NOT: {
+                    email: session.user.email
+                }
+            }
+        })
+        return user
+    } catch (error:any) {
+        return []
+    }
+}
+
+export default getUser
